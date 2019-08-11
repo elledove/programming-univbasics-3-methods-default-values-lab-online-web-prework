3163b011cb986edf50dacc4b9a185490cb708ec8
Lauren Dove <elledoveinc@gmail.com>
2019-08-11 01:28:06 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,8 +1,8 @@
 def meal_choice(veg1 , veg2 , protein = "meat")
- puts "What a nutritious meal!"
- puts "A plate of #{protein} with #{veg1} and #{veg2}."
- print "A plate of #{protein} with #{veg1} and #{veg2}"
-meal_choice
+  "What a nutritious meal!"
+  "A plate of #{protein} with #{veg1} and #{veg2}."
+  "A plate of #{protein} with #{veg1} and #{veg2}"
+
 end # Your code here
 
 meal_choice
